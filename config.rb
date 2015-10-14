dddd83adc2e131a2a2296eb8200745b9c7bf0f90
Katie Cann <katietaylorcann@gmail.com>
2015-10-13 19:39:58 -0700

    Sass Added

---
+++ config.rb
@@ -0,0 +1,17 @@
+# The starting contents for config.rb
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css" # Set this to the directory of your current css files:
+sass_dir = "assets/sass"
+images_dir = "assets/images" # Set this to the directory of your current images files:
+javascripts_dir = "assets/js" # Set this to the directory of your current JavaScript files:
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
